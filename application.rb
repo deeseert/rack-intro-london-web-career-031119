f3400d8b4283eb0a25514a2f870e376bd8ab83f0
deeseert <gio.amms@gmail.com>
2019-03-31 12:12:02 +0100

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World! My name is Giobot and I'm gonna be the coolest web server ever!! Muahahahahha!!!!"
     resp.finish
   end
 
 end
-
